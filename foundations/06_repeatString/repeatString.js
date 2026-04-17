c27cb61f38a6830fe149597e018430afc0f5c8be
GlitchiKun <p.charlie17@gmail.com>
2026-04-17 13:46:30 +0200

    Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, occurence) {
+  if (occurence < 0) {
+    return "ERROR";
+  }
 
+  let result = "";
+
+  for (let i = 0; i < occurence; i++) {
+    result += string;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
