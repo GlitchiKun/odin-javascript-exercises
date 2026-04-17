53fe1e81871ca44bd643284184f0b0ed48ee7758
GlitchiKun <p.charlie17@gmail.com>
2026-04-17 14:36:04 +0200

    Complete 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return Number(((fahrenheit - 32) * (5.0 / 9.0)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  return Number((celcius * (9.0 / 5.0) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
