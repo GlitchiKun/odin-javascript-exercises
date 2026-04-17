b03688cdc9f93edc67909465a1fbb75d24306083
GlitchiKun <p.charlie17@gmail.com>
2026-04-17 14:17:11 +0200

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return "ERROR";
+  }
 
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  let result = 0;
+
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
