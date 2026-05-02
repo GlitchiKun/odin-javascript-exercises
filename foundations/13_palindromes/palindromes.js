1292d426c8c2b6883e06003114b4c5bb1e4cd6ea
GlitchiKun <p.charlie17@gmail.com>
2026-05-02 22:49:35 +0200

    Complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanString = s
+    .toLowerCase()
+    .split("")
+    .filter((c) => alphanumerical.includes(c))
+    .join("");
+  return cleanString == cleanString.split("").reverse().join("");
 };
 
 // Do not edit below this line
