1ec655a6b3d64a8f6d15adecebbf2ef79181aaaf
GlitchiKun <p.charlie17@gmail.com>
2026-05-02 23:32:38 +0200

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array
+    .map((p) => {
+      let age;
 
+      if ("yearOfDeath" in p) {
+        age = p.yearOfDeath - p.yearOfBirth;
+      } else {
+        age = new Date().getFullYear() - p.yearOfBirth;
+      }
+
+      return {
+        name: p.name,
+        age: age,
+      };
+    })
+    .sort((a, b) => a.age - b.age)
+    .at(-1);
 };
 
 // Do not edit below this line
